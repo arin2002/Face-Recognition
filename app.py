e8fd8778cc3bed4e02afd9e3220613c0f7bfb254
Arin Agarwal <arin.agarwal02@gmail.com>
2023-03-09 09:23:05 +0000

    Mar 9, 2023, 2:53 PM

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, Response
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def gen(camera):
+    while True:
+        ret, frame = camera.read()
+        # Your face detection code here
+        # ...
+        frame = cv2.imencode('.jpg', frame)[1].tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    camera = cv2.VideoCapture(0)
+    return Response(gen(camera),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
