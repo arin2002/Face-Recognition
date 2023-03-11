a70b410bdcd804b9d1d18c370c9aa8ba5bc88802
Arin Agarwal <arin.agarwal02@gmail.com>
2023-03-11 15:58:11 +0000

    Mar 11, 2023, 9:28 PM

--- app.py
+++ app.py
@@ -48,12 +48,6 @@ print("ALL encodings are done")
 # camera part for detecting faces
 camera = cv2.VideoCapture(0)
 
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-
 def gen_frames():
     while True:
         # read camera frame
