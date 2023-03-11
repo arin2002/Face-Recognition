6ab5f78200bb94e62121b97f64740cb1b7f805d7
Arin Agarwal <arin.agarwal02@gmail.com>
2023-03-11 17:53:39 +0000

    Mar 11, 2023, 11:23 PM

--- app.py
+++ app.py
@@ -105,10 +105,6 @@ def detect_face():
     if request.method == 'POST':
         # Get the uploaded file
         file = request.files['file']
-        image = face_recognition.load_image_file(file)
-        
-        face_landmarks_list = face_recognition.face_landmarks(image)
-
         
         # Load the image data from the file
         img_data = file.read()
