d9f00a972de71a6cecf13fa9d08c17d722177460
Arin Agarwal <arin.agarwal02@gmail.com>
2023-10-08 09:19:51 +0000

    Oct 8, 2023, 2:49 PM

--- temp/test.py
+++ temp/test.py
@@ -1,37 +1,36 @@
+from easyocr import Reader
 import argparse
 import cv2
-from easyocr import Reader
 
-# Parse command-line arguments
-parser = argparse.ArgumentParser(
-    description="Extract text from an image using OCR")
+# Argument parser setup
+parser = argparse.ArgumentParser()
 parser.add_argument("--image", required=True, help="Path to input image")
-parser.add_argument("--language", required=True,
-                    help="Language code (3 letter ID)")
-parser.add_argument("--output", required=True, help="Output file name")
+parser.add_argument("--langs", type=str, default="en",
+                    help="Comma-separated list of languages for OCR")
+parser.add_argument("-g", "--gpu", type=int, default=-1,
+                    help="Whether or not GPU should be used")
 args = parser.parse_args()
 
-# Read input image using OpenCV
-image_path = args.image
-image = cv2.imread(image_path)
+# Extract arguments
+langs = args.langs.split(",")
+print("[INFO] Using the following languages: {}".format(langs))
+
+# Read input image
+image = cv2.imread(args.image)
+print("[INFO] Performing OCR on input image...")
 
 # Perform OCR using EasyOCR
-langs = [args.language]
-reader = Reader(langs)
+reader = Reader(langs, gpu=args.gpu > 0)
 results = reader.readtext(image)
 
-# Extract and save text to the output file
-output_file = args.output + "-" + args.language + ".txt"
-with open(output_file, "w", encoding="utf-8") as file:
-    for (bbox, text, prob) in results:
-        file.write(text + "\n")
-        print("[INFO] {:.4f}: {}".format(prob, text))
-
-# Display the annotated image with bounding boxes around the detected text
+# Process results and display them on the image
 for (bbox, text, prob) in results:
+    print("[INFO] {:.4f}: {}".format(prob, text))
     (top_left, top_right, bottom_right, bottom_left) = bbox
     tl = (int(top_left[0]), int(top_left[1]))
     br = (int(bottom_right[0]), int(bottom_right[1]))
+
+    # Draw bounding box and text on the image
     cv2.rectangle(image, tl, br, (0, 0, 255), 2)
     cv2.putText(image, text, (tl[0], tl[1] - 10),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2)
