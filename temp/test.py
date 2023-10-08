fa4a647d8fafce9a7326cb52f2d2f5a053e50f38
Arin Agarwal <arin.agarwal02@gmail.com>
2023-10-08 09:32:25 +0000

    Oct 8, 2023, 3:02 PM

--- temp/test.py
+++ temp/test.py
@@ -1,37 +1,51 @@
-from easyocr import Reader
-import argparse
+import tkinter as tk
+from tkinter import filedialog
 import cv2
+from easyocr import Reader
 
-# Argument parser setup
-parser = argparse.ArgumentParser()
-parser.add_argument("--image", required=True, help="Path to input image")
-parser.add_argument("--langs", type=str, default="en",
-                    help="Comma-separated list of languages for OCR")
-parser.add_argument("-g", "--gpu", type=int, default=-1,
-                    help="Whether or not GPU should be used")
-args = parser.parse_args()
 
-langs = args.langs.split(",")
-print("[INFO] Using the following languages: {}".format(langs))
+class OCRApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("OCR Application")
 
-# Read input image
-image = cv2.imread(args.image)
-print("[INFO] Performing OCR on input image...")
+        self.langs_label = tk.Label(root, text="Languages:")
+        self.langs_label.pack()
 
-reader = Reader(langs, gpu=args.gpu > 0)
-results = reader.readtext(image)
+        self.langs_entry = tk.Entry(root)
+        self.langs_entry.pack()
 
-for (bbox, text, prob) in results:
-    print("[INFO] {:.4f}: {}".format(prob, text))
-    (top_left, top_right, bottom_right, bottom_left) = bbox
-    tl = (int(top_left[0]), int(top_left[1]))
-    br = (int(bottom_right[0]), int(bottom_right[1]))
+        self.browse_button = tk.Button(
+            root, text="Browse", command=self.browse_image)
+        self.browse_button.pack()
 
-    cv2.rectangle(image, tl, br, (0, 0, 255), 2)
-    cv2.putText(image, text, (tl[0], tl[1] - 10),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2)
+        self.process_button = tk.Button(
+            root, text="Process Image", command=self.process_image)
+        self.process_button.pack()
 
-# Display the processed image with bounding boxes and detected text
-cv2.imshow("Processed Image", image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    def browse_image(self):
+        file_path = filedialog.askopenfilename()
+        self.image = cv2.imread(file_path)
+
+    def process_image(self):
+        langs = self.langs_entry.get().split(",")
+        reader = Reader(langs)
+        results = reader.readtext(self.image)
+
+        for (bbox, text, prob) in results:
+            (top_left, top_right, bottom_right, bottom_left) = bbox
+            tl = (int(top_left[0]), int(top_left[1]))
+            br = (int(bottom_right[0]), int(bottom_right[1]))
+            cv2.rectangle(self.image, tl, br, (0, 0, 255), 2)
+            cv2.putText(self.image, text, (tl[0], tl[1] - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2)
+
+        cv2.imshow("Processed Image", self.image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = OCRApp(root)
+    root.mainloop()
