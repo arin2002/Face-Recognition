676bbf50ef2e8d2be17a9fdb6a6218ed1aac88b6
Arin Agarwal <arin.agarwal02@gmail.com>
2023-10-08 09:16:20 +0000

    Oct 8, 2023, 2:46 PM

--- temp/test.py
+++ temp/test.py
@@ -1,45 +1,43 @@
-from easyocr import Reader
 import argparse
 import cv2
 from PIL import Image
-import pytesseract
+from easyocr import Reader
 
-dire = input("Type full path to image you want to convert to text:")
-im = Image.open(dire)
+# Parse command-line arguments
+parser = argparse.ArgumentParser(
+    description="Extract text from an image using OCR")
+parser.add_argument("--image", required=True, help="Path to input image")
+parser.add_argument("--language", required=True,
+                    help="Language code (3 letter ID)")
+parser.add_argument("--output", required=True, help="Output file name")
+args = parser.parse_args()
 
-language = input("Type the language of the scanned document (3 letter ID):")
-text = pytesseract.image_to_string(im, lang=language)
-filename = input("Type output file name:")
-file1 = open(filename+"-"+language+".txt", "w")
-file1.write(text)
-file1.close()
-print("Done...")
+# Read input image using OpenCV
+image_path = args.image
+image = cv2.imread(image_path)
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--image", required=True,
-                    help="path to input image")
-parser.add_argument("--langs", type=str, default="en",
-                    help="coma separated list of languages for our OCR")
-parser.add_argument("-g", "--gpu", type=int, default=-1,
-                    help="whether or not GPU should be used")
-args = vars(parser.parse_args())
-langs = args["langs"].split(",")
-print("[INFO] Using the following languages: {}".format(langs))
-image = cv2.imread(args["image"])
-print("[INFO] Performing OCR on input image...")
-reader = Reader(langs, gpu=args["gpu"] > 0)
+# Perform OCR using EasyOCR
+langs = [args.language]
+reader = Reader(langs)
 results = reader.readtext(image)
+
+# Extract and save text to the output file
+output_file = args.output + "-" + args.language + ".txt"
+with open(output_file, "w", encoding="utf-8") as file:
+    for (bbox, text, prob) in results:
+        file.write(text + "\n")
+        print("[INFO] {:.4f}: {}".format(prob, text))
+
+# Display the annotated image with bounding boxes around the detected text
 for (bbox, text, prob) in results:
-    print("[INFO] {:.4f}: {}".format(prob, text))
     (top_left, top_right, bottom_right, bottom_left) = bbox
     tl = (int(top_left[0]), int(top_left[1]))
-    tr = (int(top_right[0]), int(top_right[1]))
     br = (int(bottom_right[0]), int(bottom_right[1]))
-    bl = (int(bottom_left[0]), int(bottom_left[1]))
-
     cv2.rectangle(image, tl, br, (0, 0, 255), 2)
-    cv2.putText(image, text, (tl[0], tl[1]-10),
+    cv2.putText(image, text, (tl[0], tl[1] - 10),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2)
 
+# Display the annotated image
 cv2.imshow("Image", image)
 cv2.waitKey(0)
+cv2.destroyAllWindows()
