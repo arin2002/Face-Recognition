55462374d74a5bc3b5f7813f6f846cf3c450d392
Arin Agarwal <arin.agarwal02@gmail.com>
2023-10-08 09:15:37 +0000

    Oct 8, 2023, 2:45 PM

--- temp/test.py
+++ temp/test.py
@@ -1,6 +1,19 @@
 from easyocr import Reader
 import argparse
 import cv2
+from PIL import Image
+import pytesseract
+
+dire = input("Type full path to image you want to convert to text:")
+im = Image.open(dire)
+
+language = input("Type the language of the scanned document (3 letter ID):")
+text = pytesseract.image_to_string(im, lang=language)
+filename = input("Type output file name:")
+file1 = open(filename+"-"+language+".txt", "w")
+file1.write(text)
+file1.close()
+print("Done...")
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--image", required=True,
